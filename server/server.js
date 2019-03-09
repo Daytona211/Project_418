b9a19602c5221c5780903334844f5a95c585323c
daytona211 <znorcross@albany.edu>
2019-03-08 23:16:54 -0500

    Added Skeleton of project

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+//const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+
+
+// = to app.js
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "/../views"));
+app.use(express.static('public'));
+
+const PORT = ('port', process.env.PORT || 3000);
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+// // routes
+app.use("/", require("./routes/index"));
+// app.use("/users", require("./routes/users"));
+// app.use("/practice", require("./routes/practice"));
