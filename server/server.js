22ad63a54bad622b43a0f80761b3719d733acbc0
daytona211 <znorcross@albany.edu>
2019-03-21 14:45:15 -0400

    restructured so we don't get 404 errors ion static content (CSS, JS etc)

--- server/server.js
+++ server/server.js
@@ -8,12 +8,11 @@ const path = require("path");
 // = to app.js
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(__dirname + '/public'));
 // app.use(expressLayouts);
 app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "/../views"));
-app.use(express.static('public'));
 
 const PORT = ('port', process.env.PORT || 3000);
 app.listen(PORT, () => {
