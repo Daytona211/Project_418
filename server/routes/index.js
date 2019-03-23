3601a265f41a3d767fb4d5f426804b8dbfb95ed8
D4ryl3ne1 <Darylehen02@gmail.com>
2019-03-22 23:55:01 -0400

    added about route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+//Home Page Route
 router.get("/", (req, res)=>{
     res.render("welcomePage");
+});
+
+//About Page Route
+router.get("/about", (req, res)=>{
+    res.render("aboutPage");
 });
 
 
