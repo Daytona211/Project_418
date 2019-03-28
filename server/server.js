7446bbeaf3383e5a59f3fc50df78e9a7262aa28a
daytona211 <znorcross@albany.edu>
2019-03-28 18:41:21 -0400

    Uploaded logo

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 // app.use(expressLayouts);
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); 
 
 app.set("views", path.join(__dirname, "/../views"));
 
