7674fc8178c1d64ba9cb9f259e52f24b0f00212a
Jared Chua <jchua@albany.edu>
2019-03-28 20:01:25 -0400

    updated for mysql

--- OurDatabaseTables.sql
+++ OurDatabaseTables.sql
@@ -1,33 +1,42 @@
 CREATE TABLE UserProfile(
-	UserProfileId int identity primary key NOT NULL,
+	UserProfileId int AUTO_INCREMENT NOT NULL,
 	Name varchar(20) NOT NULL,
 	Email varchar(30) NOT NULL,
-	Password varchar(30) NOT NULL
-)
+	Password varchar(30) NOT NULL,
+	PRIMARY KEY(UserProfileId)
+);
 
 CREATE TABLE Test(
-	TestId int identity primary key NOT NULL,
-	UserProfileId int NOT NULL foreign key references UserProfile(UserProfileId),
+	TestId int AUTO_INCREMENT NOT NULL,
+	UserProfileId int NOT NULL,
 	UserStatus varchar(15) NOT NULL,
-	UserPermission varchar(15) NOT NULL
-)
+	UserPermission varchar(15) NOT NULL,
+	PRIMARY KEY(TestId),
+	FOREIGN KEY(UserProfileId) REFERENCES UserProfile(UserProfileId)
+);
 
 CREATE TABLE Image(
-	ImageId int identity primary key NOT NULL,
-	Image varbinary(max) NOT NULL
-)
+	ImageId int AUTO_INCREMENT NOT NULL,
+	Image varbinary LONGBLOB NOT NULL,
+	PRIMARY KEY(ImageId)
+);
 
 CREATE TABLE Choices(
-	ChoicesId int identity primary key NOT NULL,
-	PossibleAnswer varchar(100) NOT NULL
-)
+	ChoicesId int AUTO_INCREMENT NOT NULL,
+	PossibleAnswer varchar(100) NOT NULL,
+	PRIMARY KEY(ChoicesId)
+);
 
 CREATE TABLE Question(
-	QuestionId int identity primary key NOT NULL,
-	TestId int NOT NULL foreign key references Test(TestId),
-	ImageId int NOT NULL foreign key references Image(ImageId),
-	ChoicesId int NOT NULL foreign key references Choices(ChoicesId),
+	QuestionId int AUTO_INCREMENT NOT NULL,
+	TestId int NOT NULL,
+	ImageId int NOT NULL,
+	ChoicesId int NOT NULL,
 	TypeOfQuestion varchar(10) NOT NULL,
 	Answer varchar(100) NOT NULL,
-	Question varchar(500) NOT NULL
-)
+	Question varchar(500) NOT NULL,
+	PRIMARY KEY(QuestionId),
+	FOREIGN KEY(TestId) REFERENCES Test(TestId),
+	FOREIGN KEY(ImageId) REFERENCES Image(ImageId),
+	FOREIGN KEY(ChoicesId) REFERENCES Choices(ChoicesId)
+);
