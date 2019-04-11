c6b6e41e70b027b5bcda57857a0447a90433a95c
JChua227 <jchua@albany.edu>
2019-04-11 11:40:23 -0400

    still testing...nothing should have changed, trying to access external js

--- server/routes/users.js
+++ server/routes/users.js
@@ -80,8 +80,7 @@ router.post('/sublogin', (req, res) => {
 	});
 });
 
-
-//is this where it goes?
+//queries question/choices
 router.get('/QuizPage', (req, res) => {
 	db.query("SELECT * FROM question JOIN choices on question.questionid=choices.questionid;",(request,results,error) => {
 		if(error) console.log(error);
