cefbfe87b291e28ae8ea3151946bc4bb75fb3c6f
JChua227 <jchua@albany.edu>
2019-04-17 23:03:22 -0400

    database refined

--- OurDatabaseTables.sql
+++ OurDatabaseTables.sql
@@ -1,50 +1,50 @@
-	CREATE TABLE UserProfile(
-		UserProfileId int AUTO_INCREMENT,
-		IsAdmin BIT NOT NULL,
-		Name varchar(20) NOT NULL,
-		Email varchar(30) NOT NULL,
-		Password varchar(30) NOT NULL,
-		PRIMARY KEY (UserProfileId)
-	);
+CREATE TABLE UserProfile(
+    UserProfileId int AUTO_INCREMENT,
+    Name varchar(20) NOT NULL,
+    Password varchar(30) NOT NULL,
+    isAdmin tinyint NOT NULL,
+    PRIMARY KEY (UserProfileId)
+);
 
-	CREATE TABLE Test(
-		TestId int AUTO_INCREMENT,
-		UserProfileId int NOT NULL,
-		UserStatus varchar(15) NOT NULL,
-		PRIMARY KEY (TestId),
-		FOREIGN KEY (UserProfileId) REFERENCES UserProfile(UserProfileId)
-	);
+CREATE TABLE Test(
+    TestId int AUTO_INCREMENT,
+    UserProfileId int NOT NULL,
+    PRIMARY KEY (TestId),
+    FOREIGN KEY (UserProfileId) REFERENCES UserProfile(UserProfileId)
+);
 
-	CREATE TABLE Question(
-		QuestionId int AUTO_INCREMENT,
-		TestId int,
-		TypeOfQuestion varchar(20) NOT NULL,
-		Answer varchar(100) NOT NULL,
-		Question varchar(500) NOT NULL,
-		PRIMARY KEY (QuestionId),
-		FOREIGN KEY (TestId) REFERENCES Test(TestId)
-	);
-	
-	CREATE TABLE Image(
-		ImageId int AUTO_INCREMENT,
-		QuestionId int NOT NULL,
-		Image BLOB NOT NULL,
-		PRIMARY KEY (ImageId),
-		FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId)
-	);
+CREATE TABLE Question(
+    QuestionId int AUTO_INCREMENT,
+    TestId int,
+    TypeOfQuestion varchar(20) NOT NULL,
+    Answer varchar(100) NOT NULL,
+    Question varchar(500) NOT NULL,
+    PRIMARY KEY (QuestionId),
+    FOREIGN KEY (TestId) REFERENCES Test(TestId)
+);
 
-	CREATE TABLE Choices(
-		ChoicesId int AUTO_INCREMENT,
-		QuestionId int NOT NULL,
-		PossibleAnswer varchar(400) NOT NULL,
-		PRIMARY KEY (ChoicesId),
-		FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId)
-	);
+CREATE TABLE Image(
+    ImageId int AUTO_INCREMENT,
+    QuestionId int NOT NULL,
+    Image BLOB NOT NULL,
+    PRIMARY KEY (ImageId),
+    FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId)
+);
 
-	CREATE TABLE Grade(
-		GradeId int AUTO_INCREMENT,
-		QuestionId int NOT NULL,
-		Grade int NOT NULL,
-		PRIMARY KEY (GradeId),
-		FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId)
-	);
+CREATE TABLE Choices(
+    ChoicesId int AUTO_INCREMENT,
+    QuestionId int NOT NULL,
+    PossibleAnswer varchar(400) NOT NULL,
+    PRIMARY KEY (ChoicesId),
+    FOREIGN KEY (QuestionId) REFERENCES Question(QuestionId)
+);
+
+CREATE TABLE Grade(
+    GradeId int AUTO_INCREMENT,
+    TestId int NOT NULL,
+    UserProfileId int NOT NULL,
+    Grade DECIMAL(5,4) NOT NULL,
+    PRIMARY KEY (GradeId),
+    FOREIGN KEY (TestId) REFERENCES Test(TestId),
+    FOREIGN KEY (UserProfileId) REFERENCES UserProfile(UserProfileId)
+);
