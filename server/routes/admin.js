318232e2bd1681b3ed162309bc984d7190263194
JChua227 <jchua@albany.edu>
2019-04-20 18:41:54 -0400

    res named twice, fixed

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -299,7 +299,7 @@ router.post("/creatingtestPage", (req, res) => {
     })
     
     for(var x=0; x<results.length; x++){
-        db.query("INSERT INTO Question(TestId,Answer,Question) VALUES (?);",[result[x],"?",],(res,res,error)=>{
+        db.query("INSERT INTO Question(TestId,Answer,Question) VALUES (?);",[result[x],"?","?"],(req,res,error)=>{
             if(error){
                 console.log(error);
                 return;
